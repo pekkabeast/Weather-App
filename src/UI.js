a97b8d6bcb87dde0ea9eeb45fe73933f6072dc8d
Victor Chan <vctrchn@hotmail.ca>
2023-04-03 08:48:04 -0400

    workin gon current weather and location

--- src/UI.js
+++ src/UI.js
@@ -3,21 +3,28 @@ import { weatherApi } from "./weather";
 const weatherTileDisplay = (async () => {
   const weatherTile = document.querySelector(".weather-tile");
   const data = await weatherApi.defaultCurrentWeather();
-  const tileTitle = document.createElement("h1");
+  const tileTitle = document.createElement("div");
   tileTitle.classList.add("tile-title");
   tileTitle.textContent = "Current Weather";
-  const tileTime = document.createElement("h2");
+  const tileTime = document.createElement("div");
   tileTime.classList.add("tile-time");
-  tileTime.textContent = data.location.localtime;
+  const time = new Date(data.location.localtime);
+  tileTime.textContent =
+    time.getHours() +
+    ":" +
+    time.getMinutes() +
+    (time.getHours() >= 12 ? "pm" : "am");
   const tileTemp = document.createElement("div");
   tileTemp.classList.add("temp-display");
   const tempIcon = new Image(100, 100);
   tempIcon.src = data.current.condition.icon;
   const tempTemp = document.createElement("div");
-  tempTemp.textContent = data.current.temp_c + "C";
+  tempTemp.textContent = data.current.temp_c + "\u00B0C";
   tileTemp.append(tempIcon, tempTemp);
+  const tempDesc = document.createElement("div");
+  tempDesc.textContent = data.current.condition.text;
 
-  weatherTile.append(tileTitle, tileTime, tileTemp);
+  weatherTile.append(tileTitle, tileTime, tileTemp, tempDesc);
 })();
 
 const pageDisplay = (() => {
